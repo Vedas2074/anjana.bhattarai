8b50e99d195c98eb3c055846a00679e32412d634
btranzana <57887063+btranzana@users.noreply.github.com>
2021-03-12 09:19:09 +0545

    title change in layout for employees

--- EMPmanage/Controllers/EmployeesController.cs
+++ EMPmanage/Controllers/EmployeesController.cs
@@ -44,13 +44,3 @@ public class EmployeesController : Controller
         return View(employeeList);
     }
 }
-public class People
-{
-    public int SN { get; set; }
-    public string Fname { get; set; }// to insert default value then we use }='ahfj';
-    public string Lname { get; set; }
-    public string Address { get; set; }
-    public char Gender { get; set; }
-    public double Salary { get; set; }
-
-}
---
+++ EMPmanage/Models/people.cs
@@ -0,0 +1,10 @@
+public class People
+{
+    public int SN { get; set; }
+    public string Fname { get; set; }// to insert default value then we use }='ahfj';
+    public string Lname { get; set; }
+    public string Address { get; set; }
+    public char Gender { get; set; }
+    public double Salary { get; set; }
+
+}
