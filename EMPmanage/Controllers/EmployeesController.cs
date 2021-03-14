02d26fda713b6572f94eeccc3aaea9be2b5fcc58
btranzana <57887063+btranzana@users.noreply.github.com>
2021-03-14 22:10:27 +0545

    form addition and validation

--- EMPmanage/Controllers/EmployeesController.cs
+++ EMPmanage/Controllers/EmployeesController.cs
@@ -11,7 +11,14 @@ public class EmployeesController : Controller
     public ActionResult Details(string firstname)
     {
         var employeeList=People.GetEmployes();
-        var employee=employeeList.FirstOrDefault(x=> x.Fname==firstname);
+        var employee=employeeList.FirstOrDefault(x=> x.Firstname==firstname);
         return View(employee);
+    }
+    public ActionResult Add(){
+        return View();
+    }
+    [HttpPost]
+    public ActionResult<string> Add(People people){
+return "record saved";
     }
 }
--- EMPmanage/Models/people.cs
+++ EMPmanage/Models/people.cs
@@ -1,26 +1,32 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public class People
 {
     public int SN { get; set; }
-    public string Fname { get; set; }// to insert default value then we use }='ahfj';
-    public string Lname { get; set; }
-    public string Address { get; set; }
-    public char Gender { get; set; }
-    public double Salary { get; set; }
-    public static List<People>GetEmployes()
-    {
-        
 
-            People emp1 = new People(){Fname = "anjana",Lname = "bhatt",Gender = 'F', Address = "dhpakhel",Salary = 32322.0 };
-//obj initializes
-        People emp2 = new People()//obj initializes
-        { SN=2,Fname = "anjana",Lname = "bhatt", Gender = 'F', Address = "dhpakhel", Salary = 32322.0 };
-        People emp3 = new People()//obj initializes
-        { SN=3,Fname = "anjana", Lname = "bhatt", Gender = 'F', Address = "dhpakhel", Salary = 32322.0 };
-        People emp4 = new People()//obj initializes
-        { SN=4, Fname = "anjana", Lname = "bhatt",Gender = 'F', Address = "dhpakhel", Salary = 32322.0};
-        List<People> employeeList = new List<People>() { emp1, emp2, emp3, emp4 };
+    [Display( Name="First Name")]
+     [Required(ErrorMessage="firstname is required")] 
+    public string Firstname { get; set; }// to insert default value then we use }
+    [Display( Name="Last Name")]
+     [Required(ErrorMessage="lastname is required")] 
+    public string Lastname { get; set; }
+   
+    public string Address { get; set; }
+
+    public char? Gender { get; set; }
+    public double? Salary { get; set; }// ?this makes nullable
+    public static List<People> GetEmployes()
+    { People emp1 = new People(){Firstname = "anjana",Lastname = "bhatt",Gender = 'F', Address = "dhpakhel",Salary = 32322.0 };
+    //obj initializes
+     People emp2 = new People()//obj initializes
+     { SN=2,Firstname = "anjana",Lastname = "bhatt", Gender = 'F', Address = "dhpakhel", Salary = 32322.0 };
+    People emp3 = new People()//obj initializes
+    { SN=3,Firstname = "anjana", Lastname = "bhatt", Gender = 'F', Address = "dhpakhel", Salary = 32322.0 };
+     People emp4 = new People()//obj initializes
+    { SN=4, Firstname = "anjana", Lastname = "bhatt",Gender = 'F', Address = "dhpakhel", Salary = 32322.0};
+    List<People> employeeList = new List<People>() { emp1, emp2, emp3, emp4 };
+        
         return employeeList;
     }
 }
